beab177676137926d97702ec8ee2738f99ac2714
bendaryy <mohamedbendary1994@gmail.com>
2022-12-21 16:52:24 +0200

    Aidy

--- EInvoicing/SubmitInvoices2.bat
+++ EInvoicing/SubmitInvoices2.bat
@@ -1,2 +1,2 @@
-C:\laragon\www\halaltop\EInvoicing\publish/EInvoicingSigner.exe  C:\laragon\www\halaltop\EInvoicing 44163087 "Egypt Trust Sealing CA"
+C:\laragon\www\halaltop\EInvoicing\publish/EInvoicingSigner.exe  C:\laragon\www\halaltop\EInvoicing 10825380 "Egypt Trust Sealing CA"
 PAUSE
